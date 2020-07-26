6d3d379f9e55a93eb135e7d95bd9a3a1b22cc55a
tennis4leon <39071014+tennis4leon@users.noreply.github.com>
2020-07-26 09:56:11 +0900

    basically done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,14 @@
 //get rid of the focus ring around 3d viewer
 let modelviewer = document.querySelector("#modelviewer")
+const gitHubButton = document.querySelector(".fa fa-github-square")
+const linkedInButton = document.querySelector(".fa fa-linkedin")
+const youtubeButton = document.querySelector(".fa fa-youtube")
 
 var sheet = new CSSStyleSheet
 sheet.replaceSync( `:focus { outline: none }`)
 modelviewer.shadowRoot.adoptedStyleSheets = [sheet];
 
+gitHubButton.addEventListener("click", function(){
+    window.open( 
+        "", "_blank");
+})
