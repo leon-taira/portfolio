7548a27e6cf2f2e98c937a00567c4dab25d3c56a
tennis4leon <39071014+tennis4leon@users.noreply.github.com>
2020-07-26 13:45:42 +0900

    erased css

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,8 @@
 //get rid of the focus ring around 3d viewer
 let modelviewer = document.querySelector("#modelviewer")
-const gitHubButton = document.querySelector(".fa fa-github-square")
-const linkedInButton = document.querySelector(".fa fa-linkedin")
-const youtubeButton = document.querySelector(".fa fa-youtube")
+const gitHubButton = document.querySelector(".fa-github-square")
+const linkedInButton = document.querySelector(".fa-linkedin")
+const youtubeButton = document.querySelector(".fa-youtube")
 
 var sheet = new CSSStyleSheet
 sheet.replaceSync( `:focus { outline: none }`)
@@ -10,5 +10,15 @@ modelviewer.shadowRoot.adoptedStyleSheets = [sheet];
 
 gitHubButton.addEventListener("click", function(){
     window.open( 
-        "", "_blank");
+        "https://github.com/leon-taira", "_blank");
 })
+linkedInButton.addEventListener("click", function(){
+    window.open( 
+        "https://www.linkedin.com/in/leon-taira-hoy/", "_blank");
+})
+youtubeButton.addEventListener("click", function(){
+    window.open( 
+        "https://github.com/leon-taira", "_blank");
+})
+
+
